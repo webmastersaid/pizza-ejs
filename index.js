125fe3c7234a3bec20715b053d8cd5983b86542c
findsaid <ifvwanoff@gmail.com>
2022-03-13 16:35:05 +0500

    update

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const express = require("express");
-const { Router } = require("express");
-const router = Router();
-const config = require("config");
-const path = require("path");
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const PORT = config.get('port') || 5000;
-
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
-
-async function start() {
-  try {
-    app.listen(PORT, () => console.log("Сервер успешно запущен..."));
-  } catch (e) {
-    console.log("Ошибка сервера...", e);
-    process.exit(1);
-  }
-}
-
-start();
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Серевер успешно запущени на порту ${ PORT }`))
